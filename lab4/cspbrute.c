0b8ec0efdf11ca9f85e7724a4f7ec19822dd2818
BhavyaBh289 <shahbhavyasp@gmail.com>
2023-09-24 19:42:22 +0530

    doneeeeeeeeeeeeeeee

--- lab4/cspbrute.c
+++ lab4/cspbrute.c
@@ -8,30 +8,36 @@ struct assiged{
     char ch;
     int val;
 };
+bool sol = false;
+int nums = 0;
 bool check(int uniqueChar, struct assiged assignlist[], char* s1, char* s2, char* s3){
-    int i1=0,i2=0,i3=0,i=0;
-    // char c;
-    while(s1[i]!="\0"){
+    int i1=0, i2=0, i3=0, i=0;
+    int k = strlen(s1);
+    while (i<k){
         i1*=10;
-        for (int j=0;j<uniqueChar;j++){
-            if (s1[i] ==assignlist[j].ch){
-                i1+=assignlist[j].val;
-                j=uniqueChar;
+        for (int j=0; j<uniqueChar; j++){
+            if (s1[i] == assignlist[j].ch){
+                i1 +=assignlist[j].val;
+                j = uniqueChar;
             }
         }
+        i++;
     }
     i=0;
-    while(s2[i]!="\0"){
+    k = strlen(s2);
+    while (i<k){
         i2*=10;
-        for (int j=0;j<uniqueChar;j++){
+        for (int j=0; j<uniqueChar; j++){
             if (s2[i] ==assignlist[j].ch){
                 i2+=assignlist[j].val;
                 j=uniqueChar;
             }
         }
+        i++;
     }
     i=0;
-    while(s3[i]!="\0"){
+    k = strlen(s3);
+    while(i<k){
         i3*=10;
         for (int j=0;j<uniqueChar;j++){
             if (s3[i] ==assignlist[j].ch){
@@ -39,12 +45,38 @@ bool check(int uniqueChar, struct assiged assignlist[], char* s1, char* s2, char
                 j=uniqueChar;
             }
         }
+        i++;
     }
     return(i3==i1+i2);
 }
 void permutate(int uniqueChar,int n,struct assiged assignlist[], char* s1, char* s2, char* s3){
-    if (uniqueChar-n == 1)
-
+    if (uniqueChar-n == 1){
+        for(int i =0;i<10;i++){
+            if(!used[i]){
+                assignlist[n].val=i;
+                if(check(uniqueChar, assignlist, s1, s2, s3)){
+                    sol = true;
+                    nums+=1;
+                    printf("solution %d \n",nums);
+                    for (int j = 0; j <= n; j++) {
+                        printf(" %c = %d\t", assignlist[j].ch, assignlist[j].val);
+                    }
+                    printf("\n");
+                    return;
+                }
+            }
+        }
+        return;
+    }
+    for(int i =0;i<10;i++){
+        if(!used[i]){
+            assignlist[n].val=i;
+            used[i]=true;
+            permutate(uniqueChar, n+1, assignlist, s1, s2, s3);
+            used[i]= false;
+        }
+    }
+    return;
 }
 
 void findcsp(char* s1, char* s2, char* s3){
@@ -55,13 +87,13 @@ void findcsp(char* s1, char* s2, char* s3){
     int freq[26] = {0};
 
     for (int i = 0; i < len1; i++) {
-        ++freq[s1[i] - "a"];
+        freq[s1[i] - (int) ('a')]+=1;
     }
     for (int i = 0; i < len2; i++) {
-        ++freq[s2[i] - "a"];
+        freq[s2[i] - (int) ('a')]+=1;
     }
     for (int i = 0; i < len3; i++) {
-        ++freq[s3[i] - "a"];
+        freq[s3[i] - (int) ('a')]+=1;
     }
     int uniqueChar = 0;
     for (int i = 0; i < 26; i++) {
@@ -69,20 +101,24 @@ void findcsp(char* s1, char* s2, char* s3){
             uniqueChar++;
         }
     }
-    // if (uniqueChar > 10) {
-    //     printf("Invalid strings\n");
-    //     return;
-    // }
     struct assiged assignlist[uniqueChar];
-    int i=0,int j=0;
+    int i=0, j=0;
     while (i<26){
         if(freq[i] > 0){
-            assignlist[j].ch= (char) (i + "a");
+            assignlist[j].ch= (char) (i + 'a');
             j++;
         }
+        i++;
     }
-    for (i=0;i<10;i++){
-        used[i]=false;
+    for (i=0; i<10; i++){
+        used[i] = false;
     }
-    permutate(uniqueChar,0,assignlist,s1,s2,s3);
+    permutate(uniqueChar, 0, assignlist, s1, s2, s3);
+}
+int main(){
+    char s1[] = "base";
+    char s2[] = "ball";
+    char s3[] = "games";
+    findcsp(s1, s2, s3);
+    return 0;
 }
